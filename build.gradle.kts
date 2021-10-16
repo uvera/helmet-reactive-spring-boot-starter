62b0aad03a57c24d7fb405734c04d8613ef3d3de
Dušan Uverić <dusanuveric@protonmail.com>
2021-10-16 21:58:54 +0200

    Drop unused dependencies
    Signed-off-by: Dušan Uverić <dusanuveric@protonmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,11 +22,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     kapt("org.springframework.boot:spring-boot-autoconfigure-processor")
     kapt("org.springframework.boot:spring-boot-configuration-processor")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
 }
